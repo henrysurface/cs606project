0055825d8ed985fbe85463465df3077f2f7edba8
henrysurface <henrysurface@tamu.edu>
2018-03-24 03:51:51 +0000

    Rspec 55.84%

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :admin do
-    name                  "Mark Brown"
-    email                 "mbrown@yahoo.com"
-    password              "foobar"
-    password_confirmation "foobar"
+    name                  "Henry"
+    email                 "hneyr@gmail.com"
+    password              "123456"
+    password_confirmation "123456"
   end 
 end
